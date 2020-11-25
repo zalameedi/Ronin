ca9647961301fc2bc1cbefe33a7cc3bc36475681
Zeid Al-Ameedi <zeid.al-ameedi@wsu.edu>
2020-11-24 22:23:11 -0800

    added major functions to game class

--- src/dev/codenmore/tilegame/Game.java
+++ src/dev/codenmore/tilegame/Game.java
@@ -2,15 +2,64 @@ package dev.codenmore.tilegame;
 
 import dev.codenmore.tilegame.display.Display;
 
-public class Game {
+public class Game implements Runnable {
 	private int height, width;
+	private Thread thread;
+	private String title;
+	private boolean running;
 	
 	public Game(String title, int width, int height)
 	{
 		this.height = height;
 		this.width = width;
-		
+		this.title = title;
+		this.running = false;
+	}
+	
+	public void init()
+	{
 		Display myDisplay = new Display(height, width, title);
 	}
+	
+	public void update()
+	{
+		
+	}
+	
+	public void render()
+	{
+		
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		init();
+		while(running)
+			update();
+			render();
+			
+		try {
+			stop();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized void start()
+	{
+		if (!running)
+			running = true;
+			thread = new Thread(thread);
+			thread.start();
+	}
+	
+	public synchronized void stop() throws InterruptedException
+	{
+		if(running)
+			running = false;
+			thread.join();
+	}
+	
 
 }
